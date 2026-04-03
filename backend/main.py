5ee9b5754dc0b3d81f1838c8c277251b42e75978
vanshita.vani <vanshita.vani@hiteshi.com>
2026-04-03 14:39:44 +0530

    fix:change prt:

--- backend/main.py
+++ backend/main.py
@@ -445,7 +445,7 @@ if __name__ == "__main__":
     uvicorn.run(
         app, 
         host="0.0.0.0", 
-        port=8000,
+        port=5007,
         timeout_keep_alive=300,  # 5 minutes
         timeout_notify=300,  # 5 minutes
         limit_max_requests=10000,
